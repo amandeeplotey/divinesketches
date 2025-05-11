160c36a0f79c9e7d386d8981d611cb3ee7dd9b5b
Amandeep Singh <135829910+amandeeplotey@users.noreply.github.com>
2025-05-11 12:05:57 +0530

    Create About.js

---
+++ pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>About Divine Sketches</h1>
+      <p>Divine Sketches is a digital platform that uses AI artistry to generate personalized soulmate sketches based on your energy, profile, and submitted details. We aim to give people a creative, heartwarming experience they can cherish. </p>
+    </div>
+  );
+}
